0a48936e35e08eca142b0eff31c4f89e170558b4
Alex Bruno Neri Humberto <alex_brunoneri@hotmail.com>
2023-03-01 21:39:37 -0300

    feat: change url

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,5 +1,5 @@
 import axiox from 'axios'
 
 export const api = axiox.create({
-  baseURL: 'https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL',
+  baseURL: 'https://economia.awesomeapi.com.br/last/',
 })
